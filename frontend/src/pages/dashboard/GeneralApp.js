d58355b932ded79be64e5160e42ebb41d27575af
cb roofing <cbroofing@cbs-MacBook-Pro.modem>
2022-02-21 16:02:56 +1000

    Changes the display to a list

--- frontend/src/pages/dashboard/GeneralApp.js
+++ frontend/src/pages/dashboard/GeneralApp.js
@@ -1,22 +1,48 @@
 import { useEffect } from "react";
+
+import { Link as RouterLink } from "react-router-dom";
 // @mui
 
-import { Container, Box } from "@mui/material";
+import {
+  Card,
+  Table,
+  TableRow,
+  TableBody,
+  TableCell,
+  Container,
+  Typography,
+  TableContainer,
+} from "@mui/material";
+
 // hooks
+import useSettings from "../../hooks/useSettings";
 
 // components
 import Page from "../../components/Page";
 
-//Job
-import { JobCard } from "src/sections/jobs";
-import { SkeletonProductItem } from "src/components/skeleton";
+import Scrollbar from "../../components/Scrollbar";
+
 // redux
 import { useDispatch, useSelector } from "../../redux/store";
 import { getJobs } from "../../redux/slices/jobs";
 
+// sections
+import { UserListHead } from "../../sections/@dashboard/user/list";
+
 // ----------------------------------------------------------------------
 
-export default function GeneralApp() {
+const TABLE_HEAD = [
+  { id: "stage", label: "Stage", alignRight: false },
+  { id: "name", label: "Name", alignRight: false },
+  { id: "phone", label: "Phone", alignRight: false },
+  { id: "email", label: "Email", alignRight: false },
+  { id: "address", label: "Address", alignRight: false },
+  { id: "" },
+];
+
+// ----------------------------------------------------------------------
+
+export default function UserList() {
   const dispatch = useDispatch();
 
   const { jobs, isLoading } = useSelector((state) => state.jobs);
@@ -25,29 +51,107 @@ export default function GeneralApp() {
     dispatch(getJobs());
   }, [dispatch]);
 
+  const { themeStretch } = useSettings();
+
   return (
-    <Page title="General: App">
-      <Container sx={{ my: 15 }}>
-        <Box
-          sx={{
-            display: "grid",
-            gap: 3,
-            gridTemplateColumns: {
-              xs: "repeat(1, 1fr)",
-              sm: "repeat(2, 1fr)",
-              md: "repeat(3, 1fr)",
-            },
-          }}
-        >
-          {(isLoading ? [...Array(12)] : jobs).map((job, index) =>
-            job ? (
-              <JobCard key={job._id} job={job} />
-            ) : (
-              <SkeletonProductItem key={index} />
-            )
-          )}
-        </Box>
+    <Page title="User: List">
+      <Container
+        maxWidth={themeStretch ? false : "lg"}
+        style={{ marginTop: "200px" }}
+      >
+        {isLoading ? (
+          <h1>Loading... Just wait 2 sec Chris</h1>
+        ) : (
+          <>
+            <h1>Leads</h1>
+            <Card>
+              <Scrollbar>
+                <TableContainer sx={{ minWidth: 800 }}>
+                  <Table>
+                    <UserListHead
+                      headLabel={TABLE_HEAD}
+                      rowCount={jobs.length}
+                    />
+                    <TableBody>
+                      {jobs
+                        .filter((job) => job.stage === "lead")
+                        .map((row) => {
+                          const { _id, stage, name, mobile, email, address } =
+                            row;
+                          return (
+                            <TableRow
+                              hover
+                              key={_id}
+                              to={`/dashboard/job/${_id}`}
+                              component={RouterLink}
+                              sx={{ textDecoration: "none" }}
+                            >
+                              <TableCell
+                                sx={{ display: "flex", alignItems: "center" }}
+                              >
+                                <Typography variant="subtitle2" noWrap>
+                                  {stage}
+                                </Typography>
+                              </TableCell>
+                              <TableCell align="left">{name}</TableCell>
+                              <TableCell align="left">{mobile}</TableCell>
+                              <TableCell align="left">{email}</TableCell>
+                              <TableCell align="left">{address}</TableCell>
+                            </TableRow>
+                          );
+                        })}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+              </Scrollbar>
+            </Card>
+            <h1>Inspections</h1>
+            <Card>
+              <Scrollbar>
+                <TableContainer sx={{ minWidth: 800 }}>
+                  <Table>
+                    <UserListHead
+                      headLabel={TABLE_HEAD}
+                      rowCount={jobs.length}
+                    />
+                    <TableBody>
+                      {jobs
+                        .filter((job) => job.stage === "inspect")
+                        .map((row) => {
+                          const { _id, stage, name, mobile, email, address } =
+                            row;
+                          return (
+                            <TableRow
+                              hover
+                              key={_id}
+                              to={`/dashboard/job/${_id}`}
+                              component={RouterLink}
+                              sx={{ textDecoration: "none" }}
+                            >
+                              <TableCell
+                                sx={{ display: "flex", alignItems: "center" }}
+                              >
+                                <Typography variant="subtitle2" noWrap>
+                                  {stage}
+                                </Typography>
+                              </TableCell>
+                              <TableCell align="left">{name}</TableCell>
+                              <TableCell align="left">{mobile}</TableCell>
+                              <TableCell align="left">{email}</TableCell>
+                              <TableCell align="left">{address}</TableCell>
+                            </TableRow>
+                          );
+                        })}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+              </Scrollbar>
+            </Card>
+          </>
+        )}
       </Container>
     </Page>
   );
 }
+
+// ----------------------------------------------------------------------
